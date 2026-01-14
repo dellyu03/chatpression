880d196fafa95aa158510561d0b5ac87ce383135
dellyu03 <seunghwan081@gmail.com>
2026-01-14 11:41:30 +0900

    feat: add chatbot intiate

--- backend/services/chatbot.py
+++ backend/services/chatbot.py
@@ -1,3 +1,23 @@
 import os
 from dotenv import load_dotenv
-from openai import OpenAI 
+from openai import OpenAI
+from typing import Dict, List
+
+load_dotenv()
+
+#API키 수정 가능
+api_key = os.getenv("OPEN_API_KEY")
+
+if not api_key:
+    raise ValueError("서버에서 API 키를 찾을 수 없음")
+
+#Client Generation
+client = OpenAI(api_key = api_key)
+
+#System Prompt
+SYSTEM_PROMPTS = {
+
+}
+
+DEFAULT_PROMPT = SYSTEM_PROMPTS["friendly"]
+
